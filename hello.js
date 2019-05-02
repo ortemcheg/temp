a11e929b1d85e5d74a5b1d331931727134b28b14
Tyoma Deev <tema@deev.ru>
2019-05-02 14:04:39 +0300

    Added the Cat class
    Cat class difines cats
    Modified the first console.log

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
-console.log('Simple hello-world-like program');
+console.log('Simple hello-world-like program. Altered the line');
 const descriptor = { hello: 'world', first: 'second' };
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+}
