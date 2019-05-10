27a1f32ed5a84024de8bfc522ad4f773d87e2cdb
Tyoma Deev <tema@deev.ru>
2019-05-10 12:33:19 +0300

    Modularized code
    Now the Cat class lives in cat_class.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
+import Cat from './cat_class.js';
+
 console.log('Simple hello-world-like program. Altered the line');
 
 const descriptor = { hello: 'world', first: 'second' };
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
-  sayMeow() {
-    return 'meow';
-  }
-}
 
 const firstCat = new Cat('Leo');
-firstCat.sayMeow();
